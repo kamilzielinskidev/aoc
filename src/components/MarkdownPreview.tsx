88108ac42eb15853d44a8d6ed5da1fa4d72e7842
kamilzielinskidev <kamilzielinskidev@gmail.com>
2025-06-16 17:07:05 +0200

    force-darkmode

--- src/components/MarkdownPreview.tsx
+++ src/components/MarkdownPreview.tsx
@@ -6,6 +6,9 @@ import MarkdownPreviewLib from "@uiw/react-markdown-preview";
 export const MarkdownPreview: FC<{ source: string }> = ({ source }) => {
   return (
     <MarkdownPreviewLib
+      wrapperElement={{
+        "data-color-mode": "dark",
+      }}
       source={source}
       style={{ backgroundColor: "unset", fontFamily: "var(--font-family)" }}
     />
